803c3af0816304fc45f6d2413692209bfcac0d34
Valentin Werner <sva.werner@gmail.com>
2022-11-01 20:14:48 +0100

    wrote parsers, full article -> sentence wise, noun parsed

---
+++ soft-data/README.md
@@ -0,0 +1,5 @@
+This code is heavily oriented on the idea of https://github.com/eventdata/UDPetrarch-for-Python3
+
+Some lines of code have been copied directly, many have been rewritten for the purpose of clearity and simplicity
+
+To run this you need to install CoreNLP and UDPipe and specify their respective paths
---
+++ soft-data/src/preprocess_articles_from_xml.sh
@@ -0,0 +1,26 @@
+FILE=C:/Users/svawe/Thesis_RelationExtraction_PoliticsNews/soft-data/src/example
+STANFORD_PROPERTY=C:/Users/svawe/UDPetrarch-for-Python3/UniversalPetrarch/preprocessing/config/StanfordCoreNLP-english.properties
+udpipePath=C:/Users/svawe/udpipe-1.0.0-bin/udpipe-1.0.0-bin/bin-win64
+languageModel=C:/Users/svawe/udpipe-1.0.0-bin/udpipe-1.0.0-bin/models/udpipe-ud-1.2-160523/english-ud-1.2-160523.udpipe
+STANFORD_CORENLP=C:/Users/svawe/stanford-corenlp-4.5.0
+FILENAME=Sample_english_doc.xml
+
+echo "Call Stanford CoreNLP to do sentence splitting..."
+java -cp "$STANFORD_CORENLP/*" -Xmx4g edu.stanford.nlp.pipeline.StanfordCoreNLP -props ${STANFORD_PROPERTY} -file ${FILE}/$FILENAME -outputFormat text -outputDirectory ${FILE}
+
+echo "Generate sentence xml file..."
+python preprocess_doc.py ${FILE}/$FILENAME
+SFILENAME=$FILENAME-sent.xml.conll
+
+echo "Call udpipe to do pos tagging and dependency parsing..."
+${udpipePath}/udpipe --tag --parse --outfile=${FILE}/$SFILENAME.predpos.pred --input=conllu $languageModel ${FILE}/$SFILENAME
+
+echo "Ouput parsed xml file..."
+PFILENAME=$FILENAME-sent.xml
+python preprocess_parse.py ${FILE}/$PFILENAME
+
+echo "Removing intermediate files..."
+rm ${FILE}/$FILENAME.out
+rm ${FILE}/$PFILENAME
+rm ${FILE}/$SFILENAME
+rm ${FILE}/$SFILENAME.predpos.pred
